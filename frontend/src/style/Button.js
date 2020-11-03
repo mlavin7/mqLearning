fa76b6f67e2fd1a3e36cbb96032cfcc343b8e36a
Romulo Nunes de Menezes Roriz <rnmroriz@gmail.com>
2020-11-03 17:54:29 +0100

    static login/registration page done

--- frontend/src/style/Button.js
+++ frontend/src/style/Button.js
@@ -2,17 +2,43 @@ import styled, { css } from 'styled-components';
 import { colors, fontSizes } from '../style/theme';
 
 export const Button = styled.button`
-	padding: 0.5rem 2rem;
+	padding: 0.8rem 2.5rem;
 	font-size: ${fontSizes.normal};
 	background: ${colors.purpleMain};
 	color: ${colors.white};
 	outline: none;
 	border: none;
-	border-radius: 0.3rem;
+	border-radius: 0.5rem;
 	transition: linear 0.15s;
 	cursor: pointer;
 
 	:hover {
 		background: ${colors.purpleMainHover};
 	}
+
+	${props =>
+		props.registerBtn &&
+		css`
+			letter-spacing: 0.1rem;
+			margin-top: 1.3rem;
+			font-weight: 300;
+			border-radius: 1.5rem;
+			:hover {
+				background: ${colors.white};
+				color: ${colors.purpleMain};
+			}
+		`}
+
+	${props =>
+		props.loginBtn &&
+		css`
+			letter-spacing: 0.1rem;
+			margin-top: 1.3rem;
+			font-weight: 300;
+			border-radius: 1.5rem;
+			:hover {
+				background: ${colors.white};
+				color: ${colors.purpleMain};
+			}
+		`}
 `;
--- frontend/src/style/Container.js
+++ frontend/src/style/Container.js
@@ -1,8 +1,17 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.section`
-	max-width: 1280px;
+	max-width: 1380px;
 	margin: auto;
 	overflow: hidden;
 	padding: 0 2rem;
+
+	${props =>
+		props.centerLoginContainer &&
+		css`
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			height: 95vh;
+		`}
 `;
