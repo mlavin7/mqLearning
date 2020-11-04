a0b1cad04d9719fc84993f3e37e77251920147f3
stefan <381-samorsin@users.noreply.gitlab.propulsion-home.ch>
2020-11-04 10:32:40 +0100

    updated MainPage with footer

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from 'react';
 import TopBar from '../components/Topbar';
+import Footer from '../components/Footer'
 
 const MainPage = () => {
 	return (
 		<>
 			<TopBar />
+			<Footer />
 		</>
 	);
 };
