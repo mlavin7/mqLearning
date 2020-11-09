962f55beb7e344724b03ba58195ab74bcf410b41
Joost Idema <jpp_idema@hotmail.com>
2020-11-09 11:31:13 +0100

    changed workshop views

--- backend/workshops/urls.py
+++ backend/workshops/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', ListWorkshopView.as_view()),
     path('<int:workshop_id>/', ListWorkshopByIDView.as_view()),
     path('reserve/<int:workshop_id>/', ReserveWorkshopView.as_view()),
-    path('user/registered/<int:id>/', ListUserRegisteredWorkshopsView.as_view()),
-    path('user/attended/<int:id>/', ListUserAttendedWorkshopsView.as_view()),
+    path('user/registered/', ListUserRegisteredWorkshopsView.as_view()),
+    path('user/attended/', ListUserAttendedWorkshopsView.as_view()),
     path('create/', CreateWorkshopView.as_view()),
     path('delete/<int:workshop_id>/', DeleteWorkshopByIDView.as_view()),
 ]
