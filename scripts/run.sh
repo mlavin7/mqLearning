8fa9b074469b5cb00c7b120860a8b965574f5028
stefan <381-samorsin@users.noreply.gitlab.propulsion-home.ch>
2020-11-16 09:58:27 +0100

    run.sh script update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --no-input
-rm -rf /frontend/build/* && cp -r /frontend_tmp/build/* /frontend/build
+rm -rf /frontend/build/* && cp -R /frontend_tmp/build/* /frontend/build
 gunicorn -w 4 -b 0.0.0.0:8000 app.wsgi:application
